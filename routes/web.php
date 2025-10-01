98fa250029b6edc3accfec4c6b92ee9dfa1b1d31
SANGSANINNN6 <anangfauzan26@gmail.com>
2025-10-01 11:05:48 +0700

    tester

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/anggota', function () {
+    return view('anggota');
+});
+
+Route::get('/anggota2', function () {
+    return view('anggota2');
+});
