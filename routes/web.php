6ff08d86e277961a02db326ed1e88fed72513261
SANGSANINNN26 <anangfauzan26@gmail.com>
2025-10-22 01:16:59 +0700

    Initial Laravel Breeze project

---
+++ routes/web.php
@@ -0,0 +1,21 @@
+<?php
+
+use App\Http\Controllers\DashboardController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/anggota', function () {
+    return view('anggota');
+});
+
+Route::get('/anggota2', function () {
+    return view('anggota2');
+});
+
+Route::get('dashboard', [DashboardController::class, 'index'])
+    ->name('dashboard');
+
+    //tester
